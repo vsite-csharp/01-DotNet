1092a152aab9dbec7fe526c0e43903e822de5ba9
Kristian Bolarić <krbolaric@vsite.hr>
2023-10-19 21:15:31 +0200

    Make module CLS compliant

--- ProvideTime/ProvideTime.cs
+++ ProvideTime/ProvideTime.cs
@@ -1,7 +1,7 @@
-﻿// TODO:030 Napraviti build projekta ProvideTime i pogledati ima li pogrešaka ili upozorenja
-// TODO:031 Napraviti build projekta VbNetForm i pogledati koju pogrešku javlja
-// TODO:032 Otkomentirati donju naredbu i napraviti build projekta te provjeriti javlja li pogreške ili upozorenja
-//[assembly:CLSCompliant(true)]
+﻿// 030 Napraviti build projekta ProvideTime i pogledati ima li pogrešaka ili upozorenja
+// 031 Napraviti build projekta VbNetForm i pogledati koju pogrešku javlja
+// 032 Otkomentirati donju naredbu i napraviti build projekta te provjeriti javlja li pogreške ili upozorenja
+[assembly:CLSCompliant(true)]
 namespace Vsite.CSharp.NetFramework
 {
     public static class ProvideTime
@@ -16,8 +16,8 @@ namespace Vsite.CSharp.NetFramework
             return DateTime.Now.ToString(); 
         }
 
-        // TODO:033 Promijeniti pravo pristupa donje metode iz public u private te napraviti build projekta VbNetForm, pokrenuti program i provjeriti funkcionalnost.
-        public static string GetDatetime()
+        //033 Promijeniti pravo pristupa donje metode iz public u private te napraviti build projekta VbNetForm, pokrenuti program i provjeriti funkcionalnost.
+        private static string GetDatetime()
         {
             return DateTime.UtcNow.ToLongDateString();
         }
