70fec67c953bf86a53c3fb138de7668900d25898
DrHrast <petar.huljek@gmail.com>
2023-10-19 20:41:00 +0200

    Analize .net

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Vsite.CSharp.NetFramework
 {
-    // TODO:000 Pogledati strukturu projekta: Properties, Dependencies...
-    // TODO:001 Kliknuti na projekt desnom tipkom miša i iz kontekstnog izbornika izabrati "Open Folder in File Explorer". Pogledati strukturu kazala.
-    // TODO:002 Napraviti build programa i pogledati koja kazala i datoteke je Visual Studio kreirao.
-    // TODO:003 Pokrenuti program i pogledati ispis.
-    // TODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
+    // ODO:000 Pogledati strukturu projekta: Properties, Dependencies...
+    // ODO:001 Kliknuti na projekt desnom tipkom miša i iz kontekstnog izbornika izabrati "Open Folder in File Explorer". Pogledati strukturu kazala.
+    // ODO:002 Napraviti build programa i pogledati koja kazala i datoteke je Visual Studio kreirao.
+    // ODO:003 Pokrenuti program i pogledati ispis.
+    // ODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
     static class Program
     {
         static void Main(string[] args)
