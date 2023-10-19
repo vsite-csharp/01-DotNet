ad311762fafaeda6c28241e3938b744c5075f97b
Julijan Sribar <julijan.sribar@gmail.com>
2023-10-19 20:40:14 +0200

    Analyze .NET application executable

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Vsite.CSharp.NetFramework
 {
-    // TODO:000 Pogledati strukturu projekta: Properties, Dependencies...
-    // TODO:001 Kliknuti na projekt desnom tipkom miša i iz kontekstnog izbornika izabrati "Open Folder in File Explorer". Pogledati strukturu kazala.
-    // TODO:002 Napraviti build programa i pogledati koja kazala i datoteke je Visual Studio kreirao.
-    // TODO:003 Pokrenuti program i pogledati ispis.
-    // TODO:004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
+    // 000 Pogledati strukturu projekta: Properties, Dependencies...
+    // 001 Kliknuti na projekt desnom tipkom miša i iz kontekstnog izbornika izabrati "Open Folder in File Explorer". Pogledati strukturu kazala.
+    // 002 Napraviti build programa i pogledati koja kazala i datoteke je Visual Studio kreirao.
+    // 003 Pokrenuti program i pogledati ispis.
+    // 004 Pomoću programa ILDasm (obično unutar "C:\Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N.N Tools") pogledati sadržaj generirane izvedbene datoteke.
     static class Program
     {
         static void Main(string[] args)
